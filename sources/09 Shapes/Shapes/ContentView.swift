8957f0756dea0afc34746984a21192651a07633e
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2021-11-24 13:18:16 +0100

    Add bar chart

--- sources/09 Shapes/Shapes/ContentView.swift
+++ sources/09 Shapes/Shapes/ContentView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var values: [[CGFloat]] = (0..<7).map { _ in
+        (0..<3).map { _ in CGFloat.random(in: 0...1) }
+    }
+
     var body: some View {
-        Text("Hello, world")
+        GeometryReader { proxy in
+            HStack(alignment: .bottom, spacing: 7) {
+                ForEach(0..<values.count) { day in
+                    HStack(alignment: .bottom, spacing: 1) {
+                        ForEach(0..<values[0].count) { i in
+                            Rectangle()
+                                .fill(.red)
+                                .frame(height: proxy.size.height * values[day][i])
+                        }
+                    }
+                }
+            }
+            .frame(height: proxy.size.height)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.fixed(width: 400, height: 200))
+            .padding()
     }
 }
