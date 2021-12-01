0d80f682425865093471210d237fdd5c17e9db30
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2021-12-01 14:51:26 +0100

    Add drag gesture

--- sources/10 Gestures/FITstagram/FITstagram/EmojiView.swift
+++ sources/10 Gestures/FITstagram/FITstagram/EmojiView.swift
@@ -28,11 +28,38 @@ struct EmojiView: View {
 
     @State private var isSelected = false
 
+    @GestureState private var currentOffset: CGSize = .zero
+    @State private var offset: CGSize = .zero
+
     var body: some View {
         Text(emoji)
             .selectedFrame(isSelected)
+            .offset(offset + currentOffset)
             .onTapGesture {
                 isSelected.toggle()
             }
+            .gesture(isSelected ? dragGesture : nil)
     }
+
+    private var dragGesture: some Gesture {
+        DragGesture()
+            .updating($currentOffset) { value, state, _ in
+                state = value.translation
+            }
+//            .onChanged {
+//                currentOffset = $0.translation
+//            }
+            .onEnded {
+                offset += $0.translation
+            }
+    }
+}
+
+func += (lhs: inout CGSize, rhs: CGSize) {
+    lhs.width += rhs.width
+    lhs.height += rhs.height
+}
+
+func + (lhs: CGSize, rhs: CGSize) -> CGSize {
+    .init(width: lhs.width + rhs.width, height: rhs.height + lhs.height)
 }
